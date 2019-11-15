f1cd6be50ab2bcb4cf5976ea6ee84770de284cb1
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-15 16:10:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,5 +8,6 @@ class Person
 weight:, handed:, complexion:, t_shirt_size:, 
 wrist_size:, glove_size:, pant_length:, pant_width:})
   attributes.each {|key, value| self.send(("#{key}="), value)}
+  end 
   
 end
