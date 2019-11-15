3748807bb621fa0bbe633e24804f5d96e5378825
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-15 16:08:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,6 +7,6 @@ class Person
   def initialize(name:, birthday:, hair_color:, eye_color:, height:, 
 weight:, handed:, complexion:, t_shirt_size:, 
 wrist_size:, glove_size:, pant_length:, pant_width:)
-  
+  attributes.each {|key, value| self.send(("#{key}="), value)}
   
 end
