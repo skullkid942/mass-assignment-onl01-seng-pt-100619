9340bc1eadcede6b0042ffe575cd6335c7d04166
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-15 16:14:56 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,9 +4,9 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(attributes = {:name => nil, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width})
+  def initialize(attributes = {:name => nil, :birthday => nil, :hair_color => nil, :eye_color => nil, :height => nil, 
+:weight => nil, :handed => nil, :complexion => nil, :t_shirt_size => nil, 
+:wrist_size => nil, :glove_size => nil, :pant_length => nil, :pant_width => nil})
   attributes.each {|key, value| self.send(("#{key}="), value)}
   end 
   
